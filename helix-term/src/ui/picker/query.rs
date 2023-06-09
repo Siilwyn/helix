b76fe8b8f504141a3c7db6a07cd7b27e099556f7
Michael Davis <mcarsondavis@gmail.com>
2023-06-09 10:24:21 -0500

    WIP: Query: remove extra whitespace

--- helix-term/src/ui/picker/query.rs
+++ helix-term/src/ui/picker/query.rs
@@ -17,7 +17,7 @@ impl Query {
         let mut common_indices: HashSet<usize> = (0..field_names.len()).collect();
         let mut fields: HashMap<&str, (usize, String)> = HashMap::new();
 
-        for token in input.split_ascii_whitespace() {
+        for token in input.trim().split_ascii_whitespace() {
             match token.split_once(':') {
                 Some((key, value)) if !key.is_empty() => {
                     if let Some((_index, existing_value)) = fields.get_mut(key) {
@@ -31,12 +31,16 @@ impl Query {
                         common_indices.remove(&index);
                     } else {
                         // If the field is not valid, treat the text as common.
-                        common.push(' ');
+                        if !common.is_empty() {
+                            common.push(' ');
+                        }
                         common.push_str(token);
                     }
                 }
                 _ => {
-                    common.push(' ');
+                    if !common.is_empty() {
+                        common.push(' ');
+                    }
                     common.push_str(token);
                 }
             }
